b72ff22118619312785641bf107b03b61cd97811
Frédéric Robert <frederic.robert.axa@gmail.com>
2023-12-03 17:10:00 +0100

    Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,68 @@
+fun main() {
+    fun part1(input: List<String>) = input
+        .findAdjacentNumberAndSymbol().sumOf { it.number.value }
+
+    fun part2(input: List<String>) = input
+        .findAdjacentNumberAndSymbol()
+        .asSequence()
+        .filter { it.symbol.symbol == '*' }
+        .groupBy { it.symbol }
+        .filter { it.value.size == 2 }
+        .map { it.value
+            .map { a -> a.number.value }
+            .reduce(Int::times)
+        }
+        .sum()
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
+
+    val input = readInput("Day03")
+    part1(input).println()
+    part2(input).println()
+}
+
+data class Number(val value: Int, val col: Int, val row: Int)
+
+data class Symbol(val symbol: Char, val col: Int, val row: Int)
+
+data class Adjacent(val number: Number, val symbol: Symbol)
+
+private fun List<String>.findAdjacentNumberAndSymbol() = buildSet {
+    val numberRegex = """(\d+)""".toRegex()
+
+    for ((y, line) in this@findAdjacentNumberAndSymbol.withIndex()) {
+        val matches = numberRegex.findAll(line)
+
+        for (match in matches) {
+            val start = match.range.first
+            val end = match.range.last
+            val number = Number(match.groupValues[1].toInt(), start, y)
+
+            addAll(adjacentSymbols(number, start, end, y))
+        }
+    }
+}
+
+private fun List<String>.adjacentSymbols(number: Number, start: Int, end: Int, row: Int) = buildSet {
+    val rowRange = ((row - 1)..(row + 1)).intersect(this@adjacentSymbols.indices)
+
+    for (y in rowRange) {
+        val line = this@adjacentSymbols[y]
+        val colRange = ((start - 1)..(start + 1))
+            .union((end - 1)..(end + 1))
+            .intersect(line.indices)
+            .distinct()
+
+        for (x in colRange) {
+            if (line[x].isSymbol()) {
+                val symbol = Symbol(line[x], x, y)
+                add(Adjacent(number, symbol))
+            }
+        }
+    }
+}
+
+private fun Char.isSymbol() = !isDigit() && this != '.'
